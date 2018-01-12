fc5c2b16b50f6525f84d284840b1c4d144a58a5c
nik <supernoodz@users.noreply.github.com>
2018-01-12 08:56:13 +0000

    latest

--- templates/create-vm.ps1.j2.ps1
+++ templates/create-vm.ps1.j2.ps1
@@ -1,11 +1,11 @@
 Import-Module VirtualMachineManager | Out-Null;
-$template = Get-SCVMTemplate -Name 'Shielded16Temp' -VMMServer localhost;
-$vmconfig = New-SCVMConfiguration -VMTemplate $template -Name 'ManageIQConfig-shielded_0002';
-$vmhost   = Get-SCVMHost -ComputerName 'HV-02.KAYAN.EG';
-Set-SCVMConfiguration -VMConfiguration $vmconfig -VMHost $vmhost -VMLocation 'I:\' | Out-Null;
+$template = Get-SCVMTemplate -Name '{{ scvmm_image }}' -VMMServer localhost;
+$vmconfig = New-SCVMConfiguration -VMTemplate $template -Name 'ManageIQConfig-{{ vm_name }}';
+$vmhost   = Get-SCVMHost -ComputerName '{{ scvmm_host }}';
+Set-SCVMConfiguration -VMConfiguration $vmconfig -VMHost $vmhost -VMLocation '{{ scvmm_datastore }}' | Out-Null;
 Update-SCVMConfiguration -VMConfiguration $vmconfig | Out-Null;
-$vm = New-SCVirtualMachine -Name 'shielded_0002' -VMConfiguration $vmconfig;
+$vm = New-SCVirtualMachine -Name '{{ vm_name }}' -VMConfiguration $vmconfig;
 Set-SCVirtualMachine -VM $vm -CPUCount 1 -DynamicMemoryEnabled $false -MemoryMB 1024 | Out-Null;
 $adapter = $vm | SCVirtualNetworkAdapter;
-Set-SCVirtualNetworkAdapter -VirtualNetworkAdapter $adapter -LogicalNetwork (Get-SCLogicalNetwork -Name 'VMs') | Out-Null;
+Set-SCVirtualNetworkAdapter -VirtualNetworkAdapter $adapter -LogicalNetwork (Get-SCLogicalNetwork -Name '{{ scvmm_network }}') | Out-Null;
 $vm | Select-Object ID | ConvertTo-Json -Compress
